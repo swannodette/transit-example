96595449e37bf7013ed0e1b728d03304d9bbbfa8
Alexander Petrov <alexpetrov.rb@gmail.com>
2014-10-21 23:13:50 +0400

    Implemented benchmark for plain edn

--- src/clj/transit_example/server.clj
+++ src/clj/transit_example/server.clj
@@ -4,7 +4,8 @@
             [ring.adapter.jetty :refer [run-jetty]]
             [ring.middleware.resource :refer [wrap-resource]]
             [bidi.bidi :refer [make-handler] :as bidi]
-            [cognitect.transit :as transit]))
+            [cognitect.transit :as transit])
+  (:use ring.middleware.edn))
 
 (defn write [x]
   (let [baos (ByteArrayOutputStream.)
@@ -25,15 +26,14 @@
 (def routes
   ["" {"/" :index
        "/transit" :transit
-       "/json" :json}])
+       "/json" :json
+       "/edn" :edn}])
 
 (defn index [req]
   (assoc (resource-response "html/index.html" {:root "public"})
     :header {"Content-Type" "text/html"}))
 
-(defn json [req]
-  {:header {"Content-Type" "application/json"}
-   :body (writev [{::foobar "bar"
+(def sample-data [{::foobar "bar"
                    ::bazbaz "woz"
                    ::fooba1 "bar"
                    ::bazba2 "woz"}
@@ -72,50 +72,19 @@
                   {::foobar 1N
                    ::bazbaz 1N
                    ::fooba1 "bar"
-                   ::bazba2 "woz"}])})
+                   ::bazba2 "woz"}])
+
+(defn json [req]
+  {:header {"Content-Type" "application/json"}
+   :body (writev sample-data)})
 
 (defn transit [req]
   {:header {"Content-Type" "application/transit+json"}
-   :body   (write [{::foobar "bar"
-                    ::bazbaz "woz"
-                    ::fooba1 "bar"
-                    ::bazba2 "woz"}
-                   {::foobar "key"
-                    ::bazbaz 1
-                    ::fooba1 "bar"
-                    ::bazba2 "woz"}
-                   {::foobar "gar"
-                    ::bazbaz [1 2 3]
-                    ::fooba1 "bar"
-                    ::bazba2 "woz"}
-                   {::foobar "cat"
-                    ::bazbaz ::classified
-                    ::fooba1 "bar"
-                    ::bazba2 "woz"}
-                   {::foobar ::zebra
-                    ::bazbaz [::telephone (java.util.Date.)]
-                    ::fooba1 "bar"
-                    ::bazba2 "woz"}
-                   {::foobar ::telephone
-                    ::bazbaz ::classified
-                    ::fooba1 "bar"
-                    ::bazba2 "woz"}
-                   {::foobar "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
-                    ::bazbaz [1 9 10 {::foobar ::bazwozwoz}]
-                    ::fooba1 "bar"
-                    ::bazba2 "woz"}
-                   {::foobar ::classified
-                    ::bazbaz ::telephone
-                    ::fooba1 "bar"
-                    ::bazba2 "woz"}
-                   {::foobar "bagof"
-                    ::bazbaz :minecraft
-                    ::fooba1 "bar"
-                    ::bazba2 "woz"}
-                   {::foobar 1N
-                    ::bazbaz 1N
-                    ::fooba1 "bar"
-                    ::bazba2 "woz"}])})
+   :body   (write sample-data)})
+
+(defn edn [req]
+  {:header {"Content-Type" "application/edn"}
+   :body (pr-str sample-data)})
 
 (defn app-routes [req]
   (let [match (bidi/match-route
@@ -126,9 +95,13 @@
       :index (index req)
       :transit (transit req)
       :json (json req)
+      :edn (edn req)
       req)))
 
 (def handler
-  (wrap-resource app-routes "public"))
+  (-> (wrap-resource app-routes "public")
+      wrap-edn-params))
 
-(defonce server (run-jetty #'handler {:port 8080 :join? false}))
+(defonce server (run-jetty #'handler {:port 8081 :join? false}))
+
+
--- src/cljs/transit_example/core.cljs
+++ src/cljs/transit_example/core.cljs
@@ -9,22 +9,28 @@
 
 (defn get-data [url cb]
   (XhrIo.send (str "/" url)
-    (fn [e]
-      (let [xhr (.-target e)]
-        (cb (.getResponseText xhr))))))
+              (fn [e]
+                (let [xhr (.-target e)]
+                   (cb (.getResponseText xhr))))))
 
 (get-data "json"
-  (fn [res]
-    (println "JSON.parse")
-    (println res)
-    (time
-      (dotimes [x 50000]
-        (.parse js/JSON res)))))
+          (fn [res]
+            (println "JSON.parse")
+            (println res)
+            (time
+             (dotimes [x 50000]
+               (.parse js/JSON res)))))
 
 (get-data "transit"
-  (fn [res]
-    (println "transit read")
-    (println res)
-    (time
-      (dotimes [x 50000]
-        (t/read r res)))))
+          (fn [res]
+            (println "transit read")
+            (println (str "Transit parsed data: " (t/read r res)))
+            (time
+             (dotimes [x 50000]
+               (t/read r res)))))
+
+(println "Gonna get edn data from server 2000 times")
+(time (dotimes [x 2000]
+        (get-data "edn"
+                  (fn [res]
+                    res))))
